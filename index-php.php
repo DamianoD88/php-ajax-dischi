6f7d02d31ed35bc72320bee7cc5946ee402eb1f5
DamianoD88 <dispe88@live.it>
2021-07-29 12:56:46 +0200

    add non funziona..

--- index-php.php
+++ index-php.php
@@ -8,80 +8,9 @@ Prima Milestone: Stampiamo i dischi solo con l’utilizzo di PHP, che stampa dir
 
 
 <?php
-// $album: [
-//     [
-//         "poster"=> "https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",
-//         "title"=> "New Jersey",
-//         "author"=> "Bon Jovi",
-//         "genre"=> "Rock",
-//         "year"=> "1988"
-//     ],
-//     [
-//         "poster"=> "https://img.discogs.com/vknPDdrqRbT92pNRX0W4I5N91jg=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-1246953-1448927086-6590.jpeg.jpg",
-//         "title"=> "Live at Wembley 86",
-//         "author"=> "Queen",
-//         "genre"=> "Pop",
-//         "year"=> "1992"
-//     ],
-//     [
-//         "poster"=> "https://images-na.ssl-images-amazon.com/images/I/41JD3CW65HL.jpg",
-//         "title"=> "Ten's Summoner's Tales",
-//         "author"=> "Sting",
-//         "genre"=> "Pop",
-//         "year"=> "1993"
-//     ],
-//     [
-//         "poster"=> "https://cdn2.jazztimes.com/2018/05/SteveGadd-800x723.jpg",
-//         "title"=> "Steve Gadd Band",
-//         "author"=> "Steve Gadd Band",
-//         "genre"=> "Jazz",
-//         "year"=> "2018"
-//     ],
-//     [
-//         "poster"=> "https://images-na.ssl-images-amazon.com/images/I/810nSIQOLiL._SY355_.jpg",
-//         "title"=> "Brave new World",
-//         "author"=> "Iron Maiden",
-//         "genre"=> "Metal",
-//         "year"=> "2000"
-//     ],
-//     [
-//         "poster"=> "https://upload.wikimedia.org/wikipedia/en/9/97/Eric_Clapton_OMCOMR.jpg",
-//         "title"=> "One more car, one more raider",
-//         "author"=> "Eric Clapton",
-//         "genre"=> "Rock",
-//         "year"=> "2002"
-//     ],
-//     [
-//         "poster"=> "https://images-na.ssl-images-amazon.com/images/I/51rggtPgmRL.jpg",
-//         "title"=> "Made in Japan",
-//         "author"=> "Deep Purple",
-//         "genre"=> "Rock",
-//         "year"=> "1972"
-//     ],
-//     [
-//         "poster"=> "https://images-na.ssl-images-amazon.com/images/I/81r3FVfNG3L._SY355_.jpg",
-//         "title"=> "And Justice for All",
-//         "author"=> "Metallica",
-//         "genre"=> "Metal",
-//         "year"=> "1988"
-//     ],
-//     [
-//         "poster"=> "https://img.discogs.com/KOBsqQwKiNKH-q927oHMyVdDzSo=/fit-in/596x596/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-6406665-1418464475-6120.jpeg.jpg",
-//         "title"=> "Hard Wired",
-//         "author"=> "Dave Weckl",
-//         "genre"=> "Jazz",
-//         "year"=> "1994"
-//     ],
-//     [
-//         "poster"=> "https://m.media-amazon.com/images/I/71K9CbNZPsL._SS500_.jpg",
-//         "title"=> "Bad",
-//         "author"=> "Michael Jacjson",
-//         "genre"=> "Pop",
-//         "year"=> "1987"
-//     ],
-// ];
 
-include 'arrayAlbum.php';
+
+require __DIR__ . '/db/arrayAlbum.php';
 
 ?>
 
@@ -91,27 +20,38 @@ include 'arrayAlbum.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dischi</title>
     <link rel="stylesheet" href="css\style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title>Document</title>
+    <!-- Vue JS -->
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
+    <!-- axios -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js" integrity="sha512-bZS47S7sPOxkjU/4Bt0zrhEtWx0y0CRkhEp8IckzK+ltifIIE9EMIMTuT/mEzoIMewUINruDBIR/jJnbguonqQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    
 </head>
 <body>
-<!-- container -->
-<div class="container">
-    <!-- row -->
-<div class="row">
-
-    <?php foreach ($album as $key => $music) { ?>
-        <div class=" d-flex  align-items-center my-3">
-            <img src="<?php echo $music["poster"]; ?>" alt="" class="img-poster">
-            <h3><?php echo $music["title"]; ?></h3>
-            <h4><?php echo $music["author"]; ?></h4>
-            <h2><?php echo $music["genre"]; ?></h2>
-            <div><?php echo $music["year"]; ?></div>
+<div id="app">
+    <img src="img/010-04.jpg" alt="">
+    <div class="dischi">
+    <?php foreach($albums as $album ) {?>
+        <div class="disco">
+            <!-- nel caso scrivessi su di una sola riga posso mettere = al posto di php senza stampare attraverso l'echo -->
+            <img src="<?= $album['poster']; ?>" alt="<?= $album ['title']; ?>">
+            <h2><?= $album['title'];?></h2>
+            <div class="dati">
+                <div><?= $album['author'];?></div>
+                <div><?= $album['genre'];?></div>
+                <div><?= $album['year'];?></div>
+            </div>
         </div>
-        
+    <?php } ?>
 
-    <?php } ?> 
+    </div>
+
+</div>
+
 
 
 </div>
